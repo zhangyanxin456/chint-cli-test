9551595d56ae60adf8d5592f30f4d5d2483e62db
zhangyanxin456 <zhangyx26454@yunrong.cn>
2024-11-29 17:07:52 +0800

    commit

--- core/exec/lib/index.js
+++ core/exec/lib/index.js
@@ -1,8 +1,10 @@
 'use strict';
 
 module.exports = exec;
-
+const Package = require('@chint-cli-test/package');
 function exec() {
+  const pkg = new Package();
+  console.log(pkg, 77777)
   console.log('Hello from exec', process.env.CLI_TARGET_PATH, 66666);
   return 'Hello from exec';
 }
