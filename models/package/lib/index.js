f4b5b81d321f79becd7ba3a9694dfca8cf05bb9b
zhangyanxin456 <zhangyx26454@yunrong.cn>
2024-12-06 11:30:49 +0800

    commit

--- models/package/lib/index.js
+++ models/package/lib/index.js
@@ -3,14 +3,17 @@
 const { getDefaultRegistry, getNpmLatestVersion } = require('@chint-cli-test/get-npm-info');
 const npminstall = require('npminstall');
 const path = require('path');
+const userHome = require("user-home");
 const pathExists = require('path-exists').sync;
 const fse = require('fs-extra');
 const fs = require('fs');
+const pkgDir = require('pkg-dir').sync;
 const log = require('@chint-cli-test/log');
 const useOriginNpm = false;
 
 class Package {
   constructor(options) {
+    console.log(path.join(userHome, process.env.CLI_HOME), process.env.CLI_HOME, 1616, userHome)
     // package的目标路径
     this.targetPath = options.targetPath;
     // 缓存package的路径
@@ -29,8 +32,7 @@ class Package {
     if (!fs.existsSync(this.storeDir)) {
       fse.mkdirpSync(this.storeDir);
     }
-    log.info(this.storeDir, 'this.packageName', this.packageName, this.packageVersion);
-    const latestVersion = await getNpmLatestVersion(this.packageName, this.packageVersion);
+    const latestVersion = await getNpmLatestVersion(this.packageName);
     if (latestVersion) {
       this.packageVersion = latestVersion;
     }
@@ -53,9 +55,75 @@ class Package {
     await this.prepare();
     return fs.existsSync(this.npmFilePath);
   }
+  getPackage(isOriginal = false) {
+    if (!isOriginal) {
+      return fse.readJsonSync(path.resolve(this.npmFilePath, 'package.json'));
+    }
+    return fse.readJsonSync(path.resolve(this.storePath, 'package.json'));
+  }
+  async getVersion() {
+    await this.prepare();
+    return await this.exists() ? this.getPackage().version : null;
+  }
+  async getLatestVersion() {
+    const version = await this.getVersion();
+    if (version) {
+      const latestVersion = await getNpmLatestVersion(this.packageName, version);
+      return latestVersion;
+    }
+    return null;
+  }
   // 更新package
-  update() {}
+  async update() {
+    const latestVersion = await this.getLatestVersion();
+    log.info(latestVersion, 7575757)
+    return npminstall({
+      root: this.targetPath,
+      storeDir: this.storePath,
+      registry: npm.getNpmRegistry(useOriginNpm),
+      pkgs: [{
+        name: this.packageName,
+        version: latestVersion,
+      }],
+    });
+  }
+  get cacheFilePath() {
+    return path.resolve(this.storeDir, `_${this.cacheFilePathPrefix}@${this.packageVersion}@${this.packageName}`);
+  }
+  
   // 获取入口文件的路径
-  getRootFilePath() {}
+  getRootFilePath() {
+    function formatPath(p) {
+      if (p && typeof p === 'string') {
+        const sep = path.sep;
+        if (sep === '/') {
+          return p;
+        } else {
+          return p.replace(/\\/g, '/');
+        }
+      }
+      return p;
+    }
+    function _getRootFile(targetPath) {
+      // 1. 获取package.json所在目录
+      const dir = pkgDir(targetPath);
+      if (dir) {
+        // 2. 读取package.json
+        const pkgFile = require(path.resolve(dir, 'package.json'));
+        console.log(pkgFile, 101)
+        // 3. 寻找main/lib
+        if (pkgFile && pkgFile.main) {
+          // 4. 路径的兼容(macOS/windows)
+          return formatPath(path.resolve(dir, pkgFile.main));
+        }
+      }
+      return null;
+    }
+    if (this.storeDir) {
+      return _getRootFile(this.cacheFilePath);
+    } else {
+      return _getRootFile(this.targetPath);
+    }
+  }
 }
 module.exports = Package;
